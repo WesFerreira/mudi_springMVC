c507f80609bbe6c09a80c03f722bba02f416e04c
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-02-27 08:54:07 -0300

    update application.properties, add the package repository and Interface PedidoRepository

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -1,10 +1,18 @@
 package br.com.alura.mvc.mudi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 public class Pedido {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nomeProduto;
     private BigDecimal valorNegociavel;
     private LocalDate dataDaEntrega;
